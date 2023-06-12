b4133a68f77379db45c895e7cdf62174e08865f1
Noa <n.venevongsos@gmail.com>
2023-06-12 13:53:03 +0200

    agglo normalisé

--- prout.r
+++ prout.r
@@ -1,1 +1,12 @@
-dadadadadad
+accident <- read.csv("stat_acc_V3.csv", sep = ";")
+
+verifier_agglo<- function(valeur){
+    if (grepl("Hors agglomération", valeur)) {
+        return(0)
+    }else if (grepl("En agglomération", valeur)){
+        return(1)
+    }
+}
+
+accident$descr_agglo <- sapply(accident$descr_agglo, verifier_agglo)
+write.table(accident, file = "stat_acc_V3.csv", sep = ";", row.names = FALSE)
