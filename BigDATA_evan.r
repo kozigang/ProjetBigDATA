7daa88dd1b4e2faedbf6d36dc575635e8f8939df
evan <evan.kozierow@gmail.com>
2023-06-12 12:06:06 +0200

    ok

--- BigDATA_evan.r
+++ BigDATA_evan.r
@@ -1,4 +1,14 @@
 chemin_fichier <- "C:\\Users\\evank\\OneDrive\\Documents\\Projet_BigData\\stat_acc_V3.csv"
 donnees <- read.csv(chemin_fichier, sep=";")
-secu <- donnees["descr_dispo_secu"] 
+secu <- data.frame(donnees["descr_dispo_secu"])
 
+verifier_ceinture_securite <- function(valeur){
+    if (grepl("Utilisation d'une ceinture de sécurité", valeur)) {
+        return(1)
+    } else{
+        return(0)
+    }
+}
+
+secu$descr_dispo_secu <- sapply(secu$descr_dispo_secu, verifier_ceinture_securite)
+print(head(secu))
