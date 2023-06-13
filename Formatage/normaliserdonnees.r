08bab3ffdb7ef32e4469647641aa76a19b76cca5
evan <evan.kozierow@gmail.com>
2023-06-13 14:06:58 +0200

    formetage

--- Formatage/normaliserdonnees.r
+++ Formatage/normaliserdonnees.r
@@ -19,4 +19,18 @@ accident$place[is.na(accident$place)] <- median(subset(accident$place, accident$
 # Changer la valeur de l'age : age - 14
 accident$age <- as.numeric(accident$age)
 accident$age <- accident$age - 14
+
+#Colonne Num_Acc en "numérique"
+accident$Num_Acc <- sapply(accident$Num_Acc, function(x) {
+  x <- gsub(",", ".", x)  # Remplace la virgule par un point décimal
+  as.numeric(x)  # Convertit en numérique
+})
+
+#Colonne id_usa en "numérique"
+accident$id_usa <- as.numeric(accident$id_usa)
+#Colonne id_code_insee en "numérique" 
+accident$id_code_insee <- as.numeric(accident$id_code_insee)
+#Colonne longitude en "numérique"
+accident$longitude <- as.numeric(accident$longitude)
+
 write.table(accident, file = "stat_acc_V3.csv", sep = ";", row.names = FALSE)
--- evan_test2.r
+++ evan_test2.r
@@ -1,24 +1,10 @@
-chemin_fichier <- "C:\\Users\\evank\\OneDrive\\Documents\\Projet_BigData\\stat_acc_V3.csv"
-chemin_sauvegarde <- "C:\\Users\\evank\\OneDrive\\Documents\\Projet_BigData\\stat_acc_V3_modifie.csv"
+chemin_fichier <- "stat_acc_V3.csv"
 accident <- read.csv(chemin_fichier, sep = ";")
 
 # an_nais
 # age
 # place
+accident$longitude <- as.numeric(accident$longitude)
 
-accident$an_nais <- as.integer(accident$an_nais)
 
-accident$an_nais[is.na(accident$an_nais)] <- median(subset(accident$an_nais, accident$an_nais != "NA"))
-
-accident$age <- as.integer(accident$age)
-
-accident$age[is.na(accident$age)] <- median(subset(accident$age, accident$age != "NA"))
-
-accident$place <- as.integer(accident$place)
-
-accident$place[is.na(accident$place)] <- median(subset(accident$place, accident$place != "NA"))
-
-# Changer la valeur de l'age : age - 14
-accident$age <- as.numeric(accident$age)
-accident$age <- accident$age - 14
-write.table(accident, file = chemin_sauvegarde, sep = ";", row.names = FALSE)
+write.table(accident, file = "C:\\Users\\evank\\OneDrive\\Documents\\Projet_BigData\\stat_acc_V3_modifie.csv", sep = ";", row.names = FALSE)
