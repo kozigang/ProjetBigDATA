dc6ac3d742dd1950176a4948448fb71c0c57e318
Noa <n.venevongsos@gmail.com>
2023-06-13 09:39:21 +0200

    Normaliser année naissance

--- normaliserdonnees.r
+++ normaliserdonnees.r
@@ -7,4 +7,5 @@ accident <- read.csv("stat_acc_V3.csv", sep = ";")
 accident$an_nais <- as.integer(accident$an_nais)
 
 accident$an_nais[is.na(accident$an_nais)] <- mean(subset(accident$an_nais, accident$an_nais != "NA"))
-write.csv(accident, "stat_acc_V3.csv", row.names = FALSE)
+
+write.table(accident, file = "stat_acc_V3.csv", sep = ";", row.names = FALSE)
