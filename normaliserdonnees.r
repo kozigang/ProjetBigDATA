1cbb7ed105b53ddccaaed3423c4451d2571e0f78
Noa <n.venevongsos@gmail.com>
2023-06-13 09:19:23 +0200

    marche pas

--- normaliserdonnees.r
+++ normaliserdonnees.r
@@ -4,13 +4,7 @@ accident <- read.csv("stat_acc_V3.csv", sep = ";")
 # age
 # place
 
-accident$an_nais <- lapply(accident$an_nais, as.integer)
+accident$an_nais <- as.integer(accident$an_nais)
 
-normalise_an_nais <- function(valeur) {
-    if (grepl("NULL", valeur)) {
-        return(mean(subset(accident$an_nais, !is.na(accident$an_nais))))
-    }
-}
-
-print(mean(subset(accident$an_nais, !is.na(accident$an_nais))))
-accident$an_nais <- sapply(accident$an_nais, normalise_an_nais)
+accident$an_nais[is.na(accident$an_nais)] <- mean(subset(accident$an_nais, accident$an_nais != "NA"))
+write.csv(accident, "stat_acc_V3.csv", row.names = FALSE)
