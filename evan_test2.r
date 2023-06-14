31c29ac00628a9a5f61c3b25e1828c393517a134
evan <evan.kozierow@gmail.com>
2023-06-14 10:13:34 +0200

    oui

--- evan_test2.r
+++ evan_test2.r
@@ -1,29 +1,20 @@
-chemin_fichier <- "stat_acc_V3.csv"
+chemin_fichier <- "stat_acc_V3_original.csv"
 accident <- read.csv(chemin_fichier, sep = ";")
 
 # an_nais
 # age
 # place
-arrondissements_lyon <- list(
-  `LYON 01` = list(latitude = 45.7674, longitude = 4.8318),
-  `LYON 02` = list(latitude = 45.7575, longitude = 4.8283),
-  `LYON 03` = list(latitude = 45.7606, longitude = 4.8424),
-  `LYON 04` = list(latitude = 45.7729, longitude = 4.8277),
-  `LYON 05` = list(latitude = 45.7570, longitude = 4.8045),
-  `LYON 06` = list(latitude = 45.7691, longitude = 4.8533),
-  `LYON 07` = list(latitude = 45.7455, longitude = 4.8487),
-  `LYON 08` = list(latitude = 45.7317, longitude = 4.8619),
-  `LYON 09` = list(latitude = 45.7682, longitude = 4.8056)
-)
+# Convertir la colonne de dates en format POSIXct
 
-for (ville in accident$ville) {
-  if (ville %in% names(arrondissements_lyon)) {
-    latitude <- arrondissements_lyon[[ville]]$latitude
-    longitude <- arrondissements_lyon[[ville]]$longitude
-    accident$latitude[accident$ville == ville] <- latitude
-    accident$longitude[accident$ville == ville] <- longitude
-  }
-}
+# Convertir la colonne de dates en format POSIXct
+accident$date <- as.POSIXct(accident$date, format = "%d/%m/%Y  %H:%M", tz = "")
+
+# Formater la date pour afficher le format souhaité
+formatted_date <- format(accident$date, format = "%d/%m/%Y %H:%M:%S")
+
+print(formatted_date[1])
 
 
-write.table(accident, file = "C:\\Users\\evank\\OneDrive\\Documents\\Projet_BigData\\stat_acc_V3_modifie.csv", sep = ";", row.names = FALSE)
+
+
+#write.table(accident, file = "C:\\Users\\evank\\OneDrive\\Documents\\Projet_BigData\\stat_acc_V3_modifie.csv", sep = ";", row.names = FALSE)
