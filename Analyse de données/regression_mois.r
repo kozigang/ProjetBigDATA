ee24c67ae0fee53693b541efe407451360990ab2
evan <evan.kozierow@gmail.com>
2023-06-14 16:12:43 +0200

    regression

--- Analyse de données/regression_mois.r
+++ Analyse de données/regression_mois.r
@@ -1,10 +1,31 @@
-chemin_fichier <- "stat_acc_V3_exploitable.csv"
+# Charger les données à partir du fichier CSV
+chemin_fichier <- "stat_acc_V3_exp.csv"
 accident <- read.csv(chemin_fichier, sep = ";")
 
-accident$mois <- format(accident$date, "%m")
-accidents_par_mois <- tapply(accident$nombre_accidents, accident$mois, sum)
+# Convertir la colonne de dates en format POSIXct
+accident$date <- as.POSIXct(accident$date, format = "%Y-%m-%d %H:%M")
 
-data_mois <- data.frame(mois = as.numeric(names(accidents_par_mois)), accidents = accidents_par_mois)
-regression_mois <- lm(accidents ~ mois, data = data_mois)
-plot(data_mois$mois, data_mois$accidents, main = "Évolution du nombre d'accidents par mois", xlab = "Mois", ylab = "Nombre d'accidents")
-abline(regression_mois, col = "red")
+# Créer une nouvelle colonne pour extraire le mois
+accident$mois <- format(accident$date, "%m")
+
+# Calculer le nombre total de jours dans chaque mois
+jours_par_mois <- tapply(accident$date, accident$mois, function(x) length(unique(format(x, "%Y-%m-%d"))))
+
+# Calculer le nombre total d'accidents par mois
+nombre_accidents_par_mois <- tapply(accident$date, accident$mois, function(x) length(x))
+
+# Calculer le taux d'accidents par mois
+taux_accidents_par_mois <- nombre_accidents_par_mois / jours_par_mois
+
+# Créer un dataframe avec les données de la régression
+donnees_reg <- data.frame(mois = as.numeric(names(taux_accidents_par_mois)), taux_accidents = taux_accidents_par_mois)
+
+# Effectuer la régression linéaire
+regression <- lm(taux_accidents ~ mois, data = donnees_reg)
+
+# Afficher les résultats de la régression
+print(summary(regression))
+
+# Tracer le graphique avec la ligne de régression
+plot(donnees_reg$mois, donnees_reg$taux_accidents, main = "Évolution du taux d'accidents par mois", xlab = "Mois", ylab = "Taux d'accidents")
+abline(regression, col = "red")
--- evan_test2.r
+++ evan_test2.r
@@ -1,8 +1,7 @@
 chemin_fichier <- "stat_acc_V3_exp.csv"
 accident <- read.csv(chemin_fichier, sep = ";")
-accident$date <- as.POSIXct(accident$date, format = "%Y-%m-%d %H:%M")
 
-print(accident$date[1])
-print(class(accident$date))
+print(accident$nombre_accidents[1])
+#print(class(accident$date))
 #write.table(accident, file = "stat_acc_V3_exp.csv", sep = ";", row.names = FALSE)
 
